aedf4c68a02830dacc90dbe8879682a129d958c5
hvietnguyen88 <hviet.nguyen88@gmail.com>
2026-03-23 12:29:37 +0200

    TVN_LT2602_Day1

--- src/main/java/tvn/lt2602/A_Intro/AaIsVowel.java
+++ src/main/java/tvn/lt2602/A_Intro/AaIsVowel.java
@@ -1,4 +1,18 @@
 package tvn.lt2602.A_Intro;
 
 public class AaIsVowel {
+    public static boolean isVowel(String charInput) {
+        if (charInput == null || charInput.length() != 1) {
+            return false;
+        }
+        String newChar = charInput.toLowerCase();
+        if (newChar.equals("a") ||
+                newChar.equals("e") ||
+                newChar.equals("i") ||
+                newChar.equals("o") ||
+                newChar.equals("u")) {
+            return true;
+        }
+        return false;
+    }
 }
--- src/test/java/tvn/lt2602/A_Intro/AaIsVowelTest.java
+++ src/test/java/tvn/lt2602/A_Intro/AaIsVowelTest.java
@@ -1,5 +1,50 @@
 package tvn.lt2602.A_Intro;
 
-public class A1_VowelUtilsTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AaIsVowelTest {
+    @Test
+    public void test001 () {
+        boolean rs1 = AaIsVowel.isVowel("a");
+        Assert.assertTrue(rs1);
+        boolean rs2 = AaIsVowel.isVowel("e");
+        Assert.assertTrue(rs2);
+        boolean rs3 = AaIsVowel.isVowel("i");
+        Assert.assertTrue(rs3);
+        boolean rs4 = AaIsVowel.isVowel("o");
+        Assert.assertTrue(rs4);
+        boolean rs5 = AaIsVowel.isVowel("u");
+    }
+
+    @Test
+    public void test002 () {
+        boolean rs1 = AaIsVowel.isVowel("A");
+        Assert.assertTrue(rs1);
+        boolean rs2 = AaIsVowel.isVowel("E");
+        Assert.assertTrue(rs2);
+        boolean rs3 = AaIsVowel.isVowel("I");
+        Assert.assertTrue(rs3);
+        boolean rs4 = AaIsVowel.isVowel("O");
+        Assert.assertTrue(rs4);
+        boolean rs5 = AaIsVowel.isVowel("U");
+        Assert.assertTrue(rs5);
+    }
+
+    @Test
+    public void test003 () {
+        boolean rs1 = AaIsVowel.isVowel("b");
+        Assert.assertFalse(rs1);
+        boolean rs2 = AaIsVowel.isVowel("");
+        Assert.assertFalse(rs2);
+        boolean rs3 = AaIsVowel.isVowel("  ");
+        Assert.assertFalse(rs3);
+        boolean rs4 = AaIsVowel.isVowel("ae");
+        Assert.assertFalse(rs4);
+        boolean rs5 = AaIsVowel.isVowel(null);
+        Assert.assertFalse(rs5);
+        boolean rs6 = AaIsVowel.isVowel("1");
+        Assert.assertFalse(rs6);
+    }
     
 }
